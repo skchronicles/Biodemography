2ec1a59110ad96f10246d961206ee074cc33d091
Skyler Kuhn <kuhnsa2@mymail.vcu.edu>
2017-08-01 17:28:56 -0400

    Update __init__.py

--- biodemography/__init__.py
+++ biodemography/__init__.py
@@ -1,1 +1,19 @@
+###################################################
+# Skyler Kuhn
+# Biodemography: Pipeline Entry Point
+# Center for the Study of Biological Complexity
+# Version 1.2
+#####################################################
 
+import pipeline
+
+
+
+
+def main():
+    """Entry point for the project's pipeline"""
+    pipeline.main()
+
+
+if __name__ == "__main__":
+    main()
